8bbb82254a9480b73204f990cbb69827cfd46673
mpw <im@mariano.pw>
2022-02-28 21:24:14 -0300

    updating auto start script

--- scripts/auto-start.sh
+++ scripts/auto-start.sh
@@ -4,10 +4,8 @@ cd /home/ubuntu
 sleep 5
 cp s3/config.toml .
 cp s3/mp4-to-gif.sh .
-cp s3/gifski .
 cp s3/imgopt-server .
 chmod +x mp4-to-gif.sh
-chmod +x gifski
 chmod +x imgopt-server
 chown ubuntu:ubuntu imgopt-server
 while true;do
